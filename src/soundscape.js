c9ce66aa9bf628ec65612c2bfd6c1b8a0deebc19
lukehayes <lukepurplehayes@gmail.com>
2024-12-11 21:36:10 +0000

    Added start of main loop

--- src/soundscape.js
+++ src/soundscape.js
@@ -12,5 +12,21 @@ canvas.height = baseHeight * baseScale;
 
 const context = canvas.getContext("2d");
 
+// ----------------------------------------------------------------------------
+// Main Loop
+// ----------------------------------------------------------------------------
+const FPS = 1000 / 60; // 16ms per frame, so 60 frames a second.
+let deltaTime = 0;
+let lastTime  = 0;
 
+function update(timestamp) {
+
+    requestAnimationFrame(update);
+    deltaTime = (timestamp - lastTime) / FPS;
+    lastTime = timestamp;
+
+    gfx.clear(context, canvas);
+}
+
+requestAnimationFrame(update);
 
